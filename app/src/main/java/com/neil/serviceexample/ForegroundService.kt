9fac8105955d8d40defa806e87ff74ff77a19c06
Tsai Cheng Han <tsaichenghan999@gmail.com>
2023-11-11 19:58:49 +0800

    Call startForeground in custom ForegroundService class

--- app/src/main/java/com/neil/serviceexample/ForegroundService.kt
+++ app/src/main/java/com/neil/serviceexample/ForegroundService.kt
@@ -1,11 +1,17 @@
 package com.neil.serviceexample
 
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.*
 
 class ForegroundService : Service() {
+
+    private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     override fun onCreate() {
         super.onCreate()
@@ -23,18 +29,41 @@ class ForegroundService : Service() {
     private fun handleIntent(intent: Intent) {
         when (ServiceState.from(flag = intent.getStringExtra(SERVICE_STATE))) {
             ServiceState.STARTED -> {
-                val stringData = intent.getStringExtra(STRING_DATA)
-                Log.d("Neil", "[NormalService] service text = $stringData")
+                startDownloadInForeground()
+                Log.d("Neil", "[NormalService] Download service started")
             }
             ServiceState.STOPPED -> {
                 stopSelf()
-                Log.d("Neil", "[NormalService] Service stopped")
+                Log.d("Neil", "[NormalService] Download service stopped")
             }
         }
     }
 
+    private fun startDownloadInForeground() {
+        val notification = buildDownloadNotification()
+        startForeground(NOTIFICATION_ID, notification)
+        startDownload()
+    }
+
+    private fun startDownload() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        serviceScope.launch {
+            (1..10).forEach { downloadProgress ->
+                delay(1000)
+                notificationManager.notify(NOTIFICATION_ID, buildDownloadNotification(progress = downloadProgress))
+            }
+        }
+    }
+
+    private fun buildDownloadNotification(progress: Int = 0) = NotificationCompat.Builder(this, FOREGROUND_DOWNLOAD_CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle("File Downloading")
+        .setContentText("progress $progress/10")
+        .build()
+
     override fun onDestroy() {
         super.onDestroy()
+        serviceScope.cancel()
         Log.d("Neil", "[NormalService] Service destroyed")
     }
 
@@ -44,8 +73,9 @@ class ForegroundService : Service() {
     }
 
     companion object {
-        const val STRING_DATA = "string data"
         const val SERVICE_STATE= "service state"
+        const val NOTIFICATION_ID = 1
+        const val FOREGROUND_DOWNLOAD_CHANNEL_ID = "foreground download channel id"
     }
 
     enum class ServiceState(val flag: String) {
